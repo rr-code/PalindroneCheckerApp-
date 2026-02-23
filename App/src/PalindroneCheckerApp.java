e797857a7ccd5f86802d141dea0694c09d0ecc10
Riya <riya271206@gmail.com>
2026-02-23 10:50:54 +0530

    Riya Added UC2-HardcodedPalindrone

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
 public class PalindroneCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to Palindrome Checker App Management system");
-        System.out.println("Modify this logic to usecase2");
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        boolean isPalindrome = true;
+
+        // Loop till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
